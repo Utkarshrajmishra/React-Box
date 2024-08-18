5bd7f460c7008534dc1050220ff23a3df210e124
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-08-18 22:55:39 +0530

    Update: Card UI

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,15 @@
 import { FC } from "react";
 import ReactIcon from "../assets/images.png";
 
-interface CardProps{
-    title:string,
-    content:string
+interface CardProps {
+  title: string;
+  content: string;
 }
 
-const Card:FC<CardProps> = ({title, content}:CardProps) => {
+const Card: FC<CardProps> = ({ title, content }: CardProps) => {
   return (
-    <section className="outline outline-1 font-inter w-[350px] md:w-[400px] outline-zinc-300 height-fit rounded-xl p-4 cursor-pointer">
-      <div className="bg-white">
+    <section className="outline outline-1 outline-zinc-300 bg-zinc-50 font-inter w-[350px] md:w-[350px]  height-fit rounded-xl  px-4 py-6 cursor-pointer">
+      <div className="">
         <div className="w-fit">
           <img src={ReactIcon} alt="" width={35} />
           <p className="font-semibold mt-2 text-[1.2rem]">{title}</p>
