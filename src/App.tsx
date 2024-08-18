0eb117ad4069f88497dad90acfcdbe58088582ae
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-08-18 13:57:39 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import BlurIn from "./components/magicui/blur-in";
+
+function App() {
+  return (
+    <>
+      <BlurIn word={"Hello World"} />
+    </>
+  );
+}
+
+export default App;
